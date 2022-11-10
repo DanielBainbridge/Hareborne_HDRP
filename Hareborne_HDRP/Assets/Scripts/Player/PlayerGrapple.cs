2bc66439ad48812e38a1a8ff5021e8a1f9aecd9e
DanielBainbridge <contact.danielbainbridge@gmail.com>
2022-11-10 22:59:10 +1100

    PlayerGrapple Modified
    Added grapple hook delay to avoid spamming, changed the way force is applied to the player when grappling to be more consistent

--- Hareborne_HDRP/Assets/Scripts/Player/PlayerGrapple.cs
+++ Hareborne_HDRP/Assets/Scripts/Player/PlayerGrapple.cs
@@ -19,38 +19,48 @@ public class PlayerGrapple : MonoBehaviour
     [HideInInspector]
     public Transform m_camera;
     [HideInInspector]
-    public float m_maxRopeDistance, m_minRopeDistance, m_hookSpeed, m_hookRigidness, m_hookPullSlow, m_massScale;
+    public float m_maxRopeDistance, m_minRopeDistance, m_hookSpeed, m_grappleCooldown, m_hookRigidness, m_hookPullSlow, m_massScale;
     [HideInInspector]
     public float m_initialPull;
     //add hang time
+    private float m_cooldownCounter;
 
     void Start()
     {
+        m_cooldownCounter = m_grappleCooldown;
+    }
+    void Update()
+    {
+        if (m_cooldownCounter < m_grappleCooldown && !IsGrappling())
+            m_cooldownCounter += Time.deltaTime;
     }
     public void StartGrapple()
     {
-        //create RaycastHit
-        RaycastHit hit;
-        // if raycast hits something that you can grapple onto
-        if (Physics.Raycast(m_camera.position, m_camera.forward, out hit, m_maxRopeDistance, m_grappleableObjects))
+        if (m_cooldownCounter >= m_grappleCooldown)
         {
-            // Spring creation
-            m_grapplePoint = hit.point;
-            m_springJoint = m_player.gameObject.AddComponent<SpringJoint>();
-            m_springJoint.autoConfigureConnectedAnchor = false;
-            m_springJoint.connectedAnchor = m_grapplePoint;
+            m_cooldownCounter = 0;
+            //create RaycastHit
+            RaycastHit hit;
+            // if raycast hits something that you can grapple onto
+            if (Physics.Raycast(m_camera.position, m_camera.forward, out hit, m_maxRopeDistance, m_grappleableObjects))
+            {
+                // Spring creation
+                m_grapplePoint = hit.point;
+                m_springJoint = m_player.gameObject.AddComponent<SpringJoint>();
+                m_springJoint.autoConfigureConnectedAnchor = false;
+                m_springJoint.connectedAnchor = m_grapplePoint;
 
-            //float distanceFromPoint = Vector3.Distance(m_player.position, m_grapplePoint);
+                //set spring to pull you all the way towards the point and add a force onto that to get an initial tug on the grapple
+                m_springJoint.maxDistance = 0.1f;
+                m_springJoint.minDistance = 0f;
+                m_player.GetComponent<Rigidbody>().AddForce((m_grapplePoint - m_player.position).normalized * m_initialPull, ForceMode.Force);
 
-            m_springJoint.maxDistance = 0f;
-            m_springJoint.minDistance = 0f;
-            m_player.GetComponent<Rigidbody>().AddForce((m_grapplePoint - m_player.position).normalized * m_initialPull * 10, ForceMode.Impulse);
 
-
-            m_springJoint.spring = m_hookRigidness;
-            m_springJoint.damper = m_hookPullSlow;
-            m_springJoint.massScale = m_massScale;
-            m_springJoint.enableCollision = true;
+                m_springJoint.spring = m_hookRigidness;
+                m_springJoint.damper = m_hookPullSlow;
+                m_springJoint.massScale = m_massScale;
+                m_springJoint.enableCollision = true;
+            }
         }
     }
     public void StopGrapple()
