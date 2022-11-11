61c34a611b3473a314e4fb641a4d82548d1c17b5
FynnCat <Fynnhb@icloud.com>
2022-11-11 16:56:08 +1100

    IDK what I did today lol. Check the changelog yourself you lazy.

---
+++ Hareborne_HDRP/Assets/Outline_gizmo.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Outline_gizmo : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    void OnDrawBoxOutline()
+    {
+        // Draw a semitransparent red cube at the transforms position
+        Gizmos.color = new Color(1, 0, 0, 0.5f);
+        Gizmos.DrawCube(transform.position, new Vector3(10000, 10000, 1000));
+        
+    }
+}
